f17a9a746f2fd6066c53b87bde2a90c3f9f3846d
Marko Haanranta <marko.haanranta@helsinki.fi>
2015-10-18 17:48:02 +0300

    added checkboxes to select restaurants from which to fetch listsand then some css to get them positioned

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
 Hyy.controller('RestaurantsController', function ($scope, restaurants) {
     $scope.restaurants = restaurants;
+    $scope.checkBoxModel = [];
 });
 Hyy.controller('RestaurantController', function ($scope, restaurant) {
     $scope.restaurant = restaurant;
